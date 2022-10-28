348383259917c026e3e7a51887d277d9816b520c
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-28 06:14:00 -0500

    Team sorting function

--- src/components/teams.js
+++ src/components/teams.js
@@ -1,7 +1,31 @@
-function Teams(){
-    return (<>
-        Teams go here
-    </>)
+import {Container, Row, Col, Accordion, Card} from 'react-bootstrap';
+
+function Teams({employees, selectedTeam, setSelectedTeam}){
+
+
+    function groupTeamMembers(){
+        const teams = {}
+
+        employees.forEach(employee =>{
+            const {teamName} = employee;
+            if(!teams[teamName])
+                teams[teamName] = {
+                    name: teamName,
+                    selected: teamName === selectedTeam,
+                    members: []
+                }
+
+            teams[teamName].members.push(employee)
+        })
+
+        return teams;
+    }
+
+    const sortedTeamMembers = groupTeamMembers();
+
+    return (<Container>
+        Teams go here...
+    </Container>)
 }
 
 export default Teams
