9e3c4e77e052c95bac8770d83d082e449afb8a32
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-29 09:13:02 -0500

    Moved default employees to their own file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
+import { defaultEmployees } from './default-employees';
 import EmployeeList from './components/employee-list';
 import ErrorPage from './components/error-page';
 import Footer from './components/footer';
@@ -11,93 +12,7 @@ import Teams from './components/teams';
 
 function App() {
     const [employees, setEmployees] = useState(
-      JSON.parse(localStorage.getItem('employees')) ||
-      [
-        {
-            id: 1,
-            fullName: "Bob Jones",
-            designation: "JavaScript Developer",
-            gender: "male",
-            teamName: "TeamA"
-        },
-        {
-            id: 2,
-            fullName: "Jill Bailey",
-            designation: "Node Developer",
-            gender: "female",
-            teamName: "TeamA"
-        },
-        {
-            id: 3,
-            fullName: "Gail Shepherd",
-            designation: "Java Developer",
-            gender: "female",
-            teamName: "TeamA"
-        },
-        {
-            id: 4,
-            fullName: "Sam Reynolds",
-            designation: "React Developer",
-            gender: "male",
-            teamName: "TeamB"
-        },
-        {
-            id: 5,
-            fullName: "David Henry",
-            designation: "DotNet Developer",
-            gender: "male",
-            teamName: "TeamB"
-        },
-        {
-            id: 6,
-            fullName: "Sarah Blake",
-            designation: "SQL Server DBA",
-            gender: "female",
-            teamName: "TeamB"
-        },
-        {
-          id: 7,
-          fullName: "James Bennet",
-          designation: "Angular Developer",
-          gender: "male",
-          teamName: "TeamC"
-        },
-        {
-          id: 8,
-          fullName: "Jessica Faye",
-          designation: "API Developer",
-          gender: "female",
-          teamName: "TeamC"
-        },
-        {
-          id: 9,
-          fullName: "Lita Stone",
-          designation: "C++ Developer",
-          gender: "female",
-          teamName: "TeamC"
-        },
-        {
-          id: 10,
-          fullName: "Daniel Young",
-          designation: "Python Developer",
-          gender: "male",
-          teamName: "TeamD"
-        },
-        {
-          id: 11,
-          fullName: "Adrian Jacobs",
-          designation: "Vue Developer",
-          gender: "male",
-          teamName: "TeamD"
-        },
-        {
-          id: 12,
-          fullName: "Devin Monroe",
-          designation: "Graphic Designer",
-          gender: "male",
-          teamName: "TeamD"
-        }
-    ]);
+      JSON.parse(localStorage.getItem('employees')) || [...defaultEmployees]);
 
     const [selectedTeam, setSelectedTeam] = useState('TeamA');
     const [showSelectedOnly, toggleShowSelected] = useState(false);
@@ -131,9 +46,7 @@ function App() {
     useEffect(() =>{
       localStorage.setItem('employees', JSON.stringify(employees));
     }, [employees])
-
-    console.log()
-
+    
   return (
     <div className="App">
       <Router>
