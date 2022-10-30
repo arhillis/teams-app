25c391e031ab581fc36beb3d8db909fc2c89a35a
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-30 07:48:56 -0500

    Playing with useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useReducer} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
@@ -11,72 +11,124 @@ import Menu from './components/menu';
 import Teams from './components/teams';
 
 function App() {
-    const [employees, setEmployees] = useState(
-      JSON.parse(localStorage.getItem('employees')) || [...defaultEmployees]);
 
-    const [selectedTeam, setSelectedTeam] = useState('TeamA');
-    const [showSelectedOnly, toggleShowSelected] = useState(false);
+  const initialState = {
+    selectedTeam: 'TeamA',
+    showSelectedOnly: false,
+    employees: JSON.parse(localStorage.getItem('employees')) || defaultEmployees,
+    selectedEmployees: null
+  };
 
-    const handleChange = e =>{
-        setSelectedTeam(e.target.value)
+  const reducer = (state, action) =>{
+    const {type, payload} = action;
+    const {selectedTeam, showSelectedOnly} = state;
+    if(type === 'SELECT_TEAM')
+      return {
+        ...state,
+        selectedTeam: payload,
+        selectedEmployees: state.employees.filter(employee => employee.teamName === payload)
+      }
+    
+    if(type === 'TOGGLE_SHOW_ONLY_SELECTED')
+      return {
+        ...state,
+        showSelectedOnly: !showSelectedOnly,
+        selectedEmployees: !showSelectedOnly ? state.employees.filter(employee => employee.teamName === selectedTeam) : state.employees
+      }
+
+    if(type === 'SELECT_EMPLOYEE'){
+      localStorage.setItem('employees', JSON.stringify(payload));
+      return {
+        ...state,
+        employees: payload,
+      }
     }
+
+    console.log('No state change')
+    return state;
+  }
+
+  const [state, dispactch] = useReducer(reducer, initialState)
+
+  
+    const handleChange = e =>{
+      dispactch({type: 'SELECT_TEAM', payload: e.target.value})
+    }
+
+   const handleSelectClick = () =>{
+    dispactch({type: 'TOGGLE_SHOW_ONLY_SELECTED'})
+   }
 
     const selectEmployee = (e) =>{
         const {id} = e.currentTarget
         
+        const {employees, selectedTeam} = state
         const newEmployeeList = employees.map(employee => employee.id === parseInt(id) ? 
                                                     employee.teamName === selectedTeam ?
                                                         {...employee, teamName: ''} : 
                                                         {...employee, teamName: selectedTeam}
                                                         : employee);
 
-        setEmployees(newEmployeeList);
-    }
-    
-    const handleSelectClick = () =>{
-        toggleShowSelected(!showSelectedOnly);
+        dispactch({type: 'SELECT_EMPLOYEE', payload: newEmployeeList})
     }
 
-    const selectedTeamMembers = employees.filter(employee => employee.teamName === selectedTeam);
-
-    const selectedEmployees = showSelectedOnly ? 
-                  [...selectedTeamMembers]
-                : [...employees];
-
-    useEffect(() =>{
-      localStorage.setItem('employees', JSON.stringify(employees));
-    }, [employees])
     
-  return (
-    <div className="App">
-      <Router>
-        <Menu />
-        <Header 
-          selectedTeam={selectedTeam}
-          teamMemberCount={selectedTeamMembers.length}
-        />
-        <Routes>
-          <Route path='/' element={
-                <EmployeeList 
-                  selectedTeam={selectedTeam}
-                  selectedEmployees={selectedEmployees}
-                  selectEmployee={selectEmployee}
+
+  return (<div className='App'>
+            <Router>
+              <Menu />
+              <Header 
+                selectedTeam={state.selectedTeam}
+                teamMemberCount={state.selectedEmployees.length}
+              />
+              <Routes>
+                <Route path='/' element={<EmployeeList 
+                  selectedTeam={state.selectedTeam}
                   handleChange={handleChange}
                   handleSelectClick={handleSelectClick}
-                />
-          }></Route>
-          <Route path='/teams' element={
-                <Teams 
-                  employees={employees}
-                  selectedTeam={selectedTeam}
-                  setSelectedTeam={setSelectedTeam}
-          />}></Route>
-          <Route path='*' element={<ErrorPage />}></Route>
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
+                  selectEmployee={selectEmployee}
+                  selectedEmployees={state.showSelectedOnly ? state.selectedEmployees : state.employees}
+                />}></Route>
+                <Route path='/teams' element={<Teams 
+                  employees={state.employees}
+                />}></Route>
+                <Route path='*' element={<ErrorPage />}></Route>
+              </Routes>
+              <Footer />
+            </Router>
+          </div>)
+  //   const [employees, setEmployees] = useState(
+  //     JSON.parse(localStorage.getItem('employees')) || [...defaultEmployees]);
+
+  //   const [selectedTeam, setSelectedTeam] = useState('TeamA');
+  //   const [showSelectedOnly, toggleShowSelected] = useState(false);
+  //   const selectedTeamMembers = employees.filter(employee => employee.teamName === selectedTeam);
+
+  //   const selectedEmployees = showSelectedOnly ? 
+  //                 [...selectedTeamMembers]
+  //               : [...employees];
+
+  //   useEffect(() =>{
+  //     localStorage.setItem('employees', JSON.stringify(employees));
+  //   }, [employees])
+
+  //                 selectedEmployees={selectedEmployees}
+  //                 
+  //                 
+  //                 handleSelectClick={handleSelectClick}
+  //               
+  //         
+  //         
+  //               
+  //                 
+  //                 selectedTeam={selectedTeam}
+  //                 setSelectedTeam={setSelectedTeam}
+  //         
+  //         
+  //       
+  //       
+  //     
+
 }
 
 export default App;
--- src/components/employee-list/index.js
+++ src/components/employee-list/index.js
@@ -3,32 +3,22 @@ import { Row, Col} from 'react-bootstrap';
 import EmployeeSelect from './employee-select';
 import EmployeeCard from './employee-card';
 
-function EmployeeList({selectedTeam, selectedEmployees, selectEmployee, handleChange, handleSelectClick}){
-    //    const  = props
-    return (<main>
-                <Row className='justify-content-center mt-3 mb-3'>
-                    <Col md={8}>
-                        <EmployeeSelect
+function EmployeeList({selectedTeam, handleChange, handleSelectClick, selectedEmployees, selectEmployee}){
+
+    return (<Row className='justify-content-center mt-3 mb-3'>
+                <Col md={8}>
+                    <EmployeeSelect
                             selectedTeam={selectedTeam} 
                             handleChange={handleChange}
                             handleSelectClick={handleSelectClick}
                         />
-                    </Col>
-                </Row>
-                <Row className='justify-content-center mt-3 mb-3'>
-                    <Col md={8}>
-                        <Row className="cards">
-                            {selectedEmployees.map(employee => 
-                                <EmployeeCard key={employee.id} employee={employee} selectEmployee={selectEmployee} selectedTeam={selectedTeam}/>
-                            )}
-                        
-                        {/* <div>
-                            
-                        </div> */}
-                        </Row>
-                    </Col>
-                </Row>
-    </main>)
+                    <Row className='justify-content-center mt-3 mb-3'>
+                        {selectedEmployees.map(employee => (
+                            <EmployeeCard key={employee.id} employee={employee} selectEmployee={selectEmployee} selectedTeam={selectedTeam}/>
+                        ))} 
+                    </Row>                       
+                </Col>                    
+            </Row>)
 }
 
 export default EmployeeList;
